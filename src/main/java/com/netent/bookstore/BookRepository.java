44762e63f8c5a2ad421b301f317472563f957002
Rishabh <rishabh.adstacks@gmail.com>
2020-06-19 20:16:10 +0530

    Added search by params option ~ tested by postman
    Signed-off-by: Rishabh <rishabh.adstacks@gmail.com>

--- src/main/java/com/netent/bookstore/BookRepository.java
+++ src/main/java/com/netent/bookstore/BookRepository.java
@@ -1,8 +1,16 @@
 package com.netent.bookstore;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(collectionResourceRel = "books", path = "book")
-public interface BookRepository extends JpaRepository<Book, Integer> {
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "books", path = "books")
+public interface BookRepository extends JpaRepository<Book, String> {
+
+    @RestResource(path = "/byParams")
+    @Query(value = "select b from Book b where b.isbn = ?1 or b.title like %?1% or b.author like %?1%")
+    List<Book> findByIsbnOrTitleLikeOrAuthorLike(String search);
 }
