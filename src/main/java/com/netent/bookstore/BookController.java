683b4daeb951c338f2e0aa4ebf37f58f92070cfe
Rishabh <rishabh.adstacks@gmail.com>
2020-06-21 02:29:03 +0530

    Added exception handler in BookController
    Signed-off-by: Rishabh <rishabh.adstacks@gmail.com>

--- src/main/java/com/netent/bookstore/BookController.java
+++ src/main/java/com/netent/bookstore/BookController.java
@@ -2,11 +2,9 @@ package com.netent.bookstore;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -19,6 +17,11 @@ public class BookController {
     @GetMapping("/getMediaCoverage/{isbn}")
     ResponseEntity<?> findByMediaCoverage(@PathVariable String isbn) {
         return bookService.findByMediaCoverage(isbn);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public final ResponseEntity<Exception> handleAllExceptions(RuntimeException ex) {
+        return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
